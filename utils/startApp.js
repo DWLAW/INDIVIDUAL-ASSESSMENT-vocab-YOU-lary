2048954527a2a9c0f896ec8445a746af13d2e2a5
Dustin L <lawson.dustin12@gmail.com>
2024-06-04 19:39:19 -0500

    create works

--- utils/startApp.js
+++ utils/startApp.js
@@ -4,12 +4,14 @@ import domBuilder from '../shared/domBuilder';
 import { getVocab } from '../api/vocabData';
 import { showVocab } from '../pages/vocab';
 import domEvents from '../events/domEvents';
+import formEvents from '../events/formEvents';
 
 const startApp = () => {
   domBuilder();
+  domEvents();
+  formEvents();
   navBar();
   logoutButton();
-  domEvents();
 
   // puts vocab on the DOM
   getVocab().then((vocab) => showVocab(vocab));
