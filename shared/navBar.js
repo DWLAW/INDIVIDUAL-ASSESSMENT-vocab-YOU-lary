a98112408de8598f537df19932cec7974cf0fb52
Dustin L <lawson.dustin12@gmail.com>
2024-06-08 09:37:58 -0500

    added styling, started on filter

--- shared/navBar.js
+++ shared/navBar.js
@@ -8,6 +8,25 @@ const navBar = () => {
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item active">
+          <a class="nav-link" href="#" id="all-vocab">
+            All Vocab <span class="sr-only">(current)</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#" id="nouns">Nouns</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#" id="verbs">Verbs</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#" id="adverbs">Adverbs</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#" id="adjectives">Adjectives</a>
+        </li>
+        </ul>
         
         <span class="navbar-text">
           <div id="logout-button"></div>
